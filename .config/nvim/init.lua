e6dad1d387170b785b924f06d87d2d98f0d645be
sowsanti <santiago.sow@hotmail.com>
2023-08-11 15:31:38 +0200

    Added FTerm plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -255,6 +255,23 @@ require("lazy").setup({
   { "catppuccin/nvim", as = "catppuccin" },
   "ellisonleao/gruvbox.nvim",
   "rebelot/kanagawa.nvim",
+  -- small term floating window
+  {
+    'numToStr/FTerm.nvim',
+    config = function()
+      local map = vim.api.nvim_set_keymap
+      local opts = { noremap = true, silent = true }
+      require 'FTerm'.setup({
+        blend = 5,
+        dimensions = {
+          height = 0.90,
+          width = 0.90,
+          x = 0.5,
+          y = 0.5
+        }
+      })
+    end
+  },
 }, {})
 
 -- [[ Setting options ]]
