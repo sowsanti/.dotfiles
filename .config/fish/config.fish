38b4fccdd576d05c7285df4f9debc547f3d6d905
sowsanti <santiago.sow@hotmail.com>
2023-08-11 22:06:25 +0200

    Reverted working tree change (I hope...)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,6 +6,6 @@ function fish_greeting
 end
 
 # aliases
-alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME/.config"
+alias dotfiles="/usr/bin/git --git-dir=$HOME/.dotfiles --work-tree=$HOME"
 alias v="nvim"
-alias vc="env GIT_DIR=$HOME/.dotfiles GIT_WORK_TREE=$HOME/.config nvim $HOME/.config"
+alias vc="env GIT_DIR=$HOME/.dotfiles GIT_WORK_TREE=$HOME nvim $HOME/.config"
