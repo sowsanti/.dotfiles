5a1d283f261e0ea9b2f23a79ea821e43271b60d1
sowsanti <santiago.sow@hotmail.com>
2023-08-16 11:02:38 +0200

    Better Noice config, removed nvim-notify.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -272,7 +272,7 @@ require("lazy").setup({
 			-- OPTIONAL:
 			--   `nvim-notify` is only needed, if you want to use the notification view.
 			--   If not available, we use `mini` as the fallback
-			"rcarriga/nvim-notify",
+			-- "rcarriga/nvim-notify",
 		},
 	},
 	{
@@ -564,7 +564,7 @@ require("noice").setup({
 		command_palette = true, -- position the cmdline and popupmenu together
 		long_message_to_split = true, -- long messages will be sent to a split
 		inc_rename = false, -- enables an input dialog for inc-rename.nvim
-		lsp_doc_border = false, -- add a border to hover docs and signature help
+		lsp_doc_border = true, -- add a border to hover docs and signature help
 	},
 })
 
